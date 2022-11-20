d67322464e91ad73998f11b23541ff97d2de7896
Enes <enesbilik8@gmail.com>
2022-11-20 22:19:26 +0300

    MVC pattern applied

---
+++ controllers/tourController.js
@@ -0,0 +1,110 @@
+const Tour = require("../models/tourModel");
+
+// const checkId = (req, res, next, _val) => {
+//   console.log("check id is working 🍓");
+//   const id = req.params.id * 1;
+// const tour = tours.find((el) => el.id === id);
+
+// if (!tour)
+//   return res.status(404).json({
+//     status: "fail",
+//     message: "Invalid Id",
+//   });
+//   next();
+// };
+
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Bad request",
+    });
+  }
+};
+
+const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+const createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
+};
+
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+      message: "Tour deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+module.exports = {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+};
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const getAllUsers = (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "This endpoint is not ready yet.",
+  });
+};
+const createUser = (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "This endpoint is not ready yet.",
+  });
+};
+const getUser = (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "This endpoint is not ready yet.",
+  });
+};
+const updateUser = (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "This endpoint is not ready yet.",
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "This endpoint is not ready yet.",
+  });
+};
+
+module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
